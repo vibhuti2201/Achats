7c661063d1c9c17dccc9c997bb1e886b0c016b86
Vibhuti Kapoor <vibhutikapoor22@gmail.com>
2023-08-15 23:31:58 +0530

    ui

---
+++ app/src/main/java/com/example/achats/activities/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.achats.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.achats.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
--- app/src/main/java/com/example/achats/activities/ShoppingActivity.kt
+++ app/src/main/java/com/example/achats/activities/ShoppingActivity.kt
@@ -1,11 +1,12 @@
-package com.example.achats
+package com.example.achats.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.achats.R
 
-class MainActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_shopping)
     }
 }
