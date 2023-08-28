e05d7f3a6c689772eea3d8aeeb87f7f06fc0b67c
Vibhuti Kapoor <vibhutikapoor22@gmail.com>
2023-08-28 23:12:07 +0530

    login

--- app/src/main/java/com/example/achats/activities/LoginRegisterActivity.kt
+++ app/src/main/java/com/example/achats/activities/LoginRegisterActivity.kt
@@ -3,7 +3,9 @@ package com.example.achats.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.achats.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/achats/fragments/RegisterFragment.kt
+++ app/src/main/java/com/example/achats/fragments/RegisterFragment.kt
@@ -1,18 +1,28 @@
 package com.example.achats.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.achats.R
+import com.example.achats.data.User
 import com.example.achats.databinding.FragmentRegisterBinding
+import com.example.achats.util.Resource
+import com.example.achats.viewmodel.RegisterViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
 
+private val TAG= "RegisterFragment"
+@AndroidEntryPoint
 class RegisterFragment : Fragment() {
 
 private lateinit var binding:FragmentRegisterBinding
-
+private  val viewModel by viewModels<RegisterViewModel>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,8 +35,33 @@ private lateinit var binding:FragmentRegisterBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
-
+         binding.buttonRegisterRegister.setOnClickListener {
+             val user= User(
+                 edFirstName.text.toString().trim(),
+                 edLastName.text.toString().trim(),
+                 edEmail.text.toString().trim()
+             )
+             val password = edPassword.text.toString()
+             viewModel.createAccountWithEmailAndPassword(user,password)
+         }
+        }
+        lifecycleScope.launchWhenStarted {
+            viewModel.register.collect{
+                when(it){
+                    is Resource.Loading -> {
+                       binding.buttonRegisterRegister.startAnimation()
+                    }
+                    is Resource.Success->{
+                         Log.d("text",it.message.toString())
+                        binding.buttonRegisterRegister.revertAnimation()
+                    }
+                    is Resource.Error->{
+                        Log.e(TAG,it.message.toString())
+                        binding.buttonRegisterRegister.revertAnimation()
+                    }
+                    else -> Unit
+                }
+            }
         }
     }
-
 }
