0666b60b49b047a4c0218fc19670f4b4d6bac058
Vibhuti Kapoor <vibhutikapoor22@gmail.com>
2023-11-25 23:36:27 +0530

    main category

--- app/src/main/java/com/example/achats/viewmodel/MainCategoryViewModel.kt
+++ app/src/main/java/com/example/achats/viewmodel/MainCategoryViewModel.kt
@@ -2,12 +2,17 @@ package com.example.achats.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.bumptech.glide.load.engine.Resource
+import com.example.achats.data.Product
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 class MainCategoryViewModel  @Inject constructor(
     private val firestore: FirebaseFirestore
 ): ViewModel(){
-  //  private val specialProducts= MutableStateFlow<Resource>()
+
+    private val _specialProducts= MutableStateFlow<Resource<List<Product>>>(Resource.Unspecified())
+    val specialProduct: StateFlow<<Resource<List<Product>>> = _specialProducts
+
 }
